9b5919fdb8edf93089ce9b6d62a1d313110f0973
Nazar <nazarshmagailo12@gmail.com>
2025-04-02 21:54:30 +0300

    Work brachA

--- FuncA.h
+++ FuncA.h
@@ -1,14 +1,13 @@
 #ifndef FUNCA_H
 #define FUNCA_H
+
 #include <iostream>
 #include <cmath>
 #include <limits>
-
 using namespace std;
 
-class FuncA {
- 
 
+class FuncA {
 public:
 
     /**
@@ -27,9 +26,18 @@ public:
 
 private:
 
+    /**
+     * 
+     * @brief Compute factorial of a number.
+     * 
+     * This function takes one integer as input, calculates its factorial, and
+     * returns the result.
+     * 
+     * @param n The number whose factorial will be calculated.
+     * 
+     */
     unsigned long long factorial(int n);
-
-
 };
+
 
 #endif
