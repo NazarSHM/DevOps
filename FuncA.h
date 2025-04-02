97513bdf513147223b95d896debeb32ef652fc05
Nazar <nazarshmagailo12@gmail.com>
2025-04-02 17:41:16 +0300

    Add formulas

---
+++ FuncA.h
@@ -0,0 +1,24 @@
+#ifndef FUNCA_H
+#define FUNCA_H
+#include <iostream>
+#include <cmath>
+#include <limits>
+
+using namespace std;
+
+class FuncA {
+ 
+
+public:
+ 
+    double taylor_sine(double x);
+
+
+private:
+
+    unsigned long long factorial(int n);
+
+
+};
+
+#endif
